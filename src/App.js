53b10d565a51257aa6bcc43cbde4061c12d77686
nastya <karpenk0nasty@yandex.by>
2021-02-07 21:10:51 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useLayoutEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
+       <ul>
+         <li>fanfiki</li>
+         <li>testy</li>
+         <li>history</li>
+       </ul>
     </div>
   );
 }
